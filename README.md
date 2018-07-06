02df315165c27dc29519f6c99a97c8936d2bece2
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-07-06 11:22:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Loan Calculator
+
+A small web application developed to calculate minimum monthly payments in rupees to meet specified interest rate and number of repayment years.
+
+![Web application with form values inputted](screenshot.jpg)
+
+## Technologies Used in this repo
+
+* JavaScript ES6: plain JS (as opposed to jQuery, etc.) is used to dynamically query, create, modify, and remove HTML elements/nodes
+* Node.js: basic web server
+* Bootstrap: front-end CSS/JS framework
+
+## Features
+
+1. A loading GIF is displayed after a user submits the form. This enhances the user experience (ever so slightly) as it informs the user that some kind of processing is occurring.
+
+2. Form validation checks user input by using JavaScript's isFinite() method. If, during calculation, an issue arises with any intermediate value, using isFinite() verifies this scenario and displays an error message at the top.
+
+   ![Error message appears if any of the form inputs are left blank](screenshot2.jpg)
+
+## Thoughts
+
+The setTimeout() method of the **window** object is a great, simple tool. In this project, it was used several times to hide and show DIVs at designated intervals.
---
+++ public/calculator.js
@@ -0,0 +1,79 @@
+// listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    // hide results
+    document.getElementById('results').style.display = 'none';
+
+    // show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 500);
+
+    e.preventDefault();
+});
+
+// calculate results
+function calculateResults() {
+    // UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    // check for finite number
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // show results
+        document.getElementById('results').style.display = 'block';
+
+        // hide loader
+        document.getElementById('loading').style.display = 'none';
+    } else {
+        showError('Please check your numbers');
+    }
+}
+
+// show an error
+function showError(error) {
+    // hide results
+    document.getElementById('results').style.display = 'none';
+
+    // hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // create a div
+    const errorDiv = document.createElement('div');
+
+    // retrieve elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add classes
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+// clear error
+function clearError() {
+    document.querySelector('.alert').remove();
+}
